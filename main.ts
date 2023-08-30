16b2b867bf981e5d3168fb2983bda52e447ec30a
musyira <143159164+musyira@users.noreply.github.com>
2023-08-30 15:33:22 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showIcon(IconNames.Yes)
     basic.pause(2000)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
 })
 radio.onReceivedString(function (receivedString) {
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("wake up")
 })
 radio.setGroup(100)
